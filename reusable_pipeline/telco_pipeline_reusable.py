e0ac5849929696caa48d60cc19a6d4b999f51027
Jabor047 <gkkarobia@gmail.com>
2022-05-11 10:50:50 +0300

    Changed the method of creating the task components

--- reusable_pipeline/telco_pipeline_reusable.py
+++ reusable_pipeline/telco_pipeline_reusable.py
@@ -1,32 +1,18 @@
 import kfp
 from kfp import dsl
 
-def feature_prep():
-    feature_prep_op = kfp.components.load_component_from_url()
-    return feature_prep_op
-
-def model_training(experience_data, engagement_data):
-    return dsl.ContainerOp(
-        name='model_training',
-        image='jabor047/kubeflow-pipeline-reusable-components-model-training:v.0.1',
-        arguments=[
-            '--experience_data', experience_data,
-            '--engagement_data', engagement_data
-        ],
-        file_outputs={
-            'model': '/model_training/satisfaction_kmeans.pkl'
-        }
-    )
-
 @dsl.pipeline(
     name='telco_pipeline_reusable',
     description='Telco Pipeline Reusable Components')
 def telco_reusable_pipeline():
-    feature_prep_op = feature_prep()
-    model_training_op = model_training(
-        experience_data=dsl.InputArgumentPath(feature_prep_op.outputs['experience_data']),
-        engagement_data=dsl.InputArgumentPath(feature_prep_op.outputs['engagement_data'])
-    ).after(feature_prep_op)
+    feature_prep_op = kfp.components.load_component_from_url(
+        "https://raw.githubusercontent.com/Jabor047/Kubeflow-Pipelines/main/reusable_pipeline/feature_prep.yaml"
+    )
+    model_training_comp = kfp.components.load_component_from_url(
+        "https://raw.githubusercontent.com/Jabor047/Kubeflow-Pipelines/main/reusable_pipeline/model_training.yaml"
+    )
+    model_training_op = model_training_comp(feature_prep_op.outputs["engagement_data"],
+                                            feature_prep_op.outputs["experience_data"]).after(feature_prep_op)
 
 kfp.compiler.Compiler().compile(telco_reusable_pipeline, 'telco_pipeline_reusable.zip')
 
