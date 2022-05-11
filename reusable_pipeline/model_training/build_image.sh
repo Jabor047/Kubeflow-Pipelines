76b7fc687022adecb8b6983c0d749e06c09e03b8
Jabor047 <gkkarobia@gmail.com>
2022-05-11 11:10:53 +0300

    added a bash script to build the docker container and push to docker hub

---
+++ reusable_pipeline/model_training/build_image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+image_name=jabor047/kubeflow-pipeline-reusable-components-model-training
+image_tag=latest
+full_image_name=${image_name}:${image_tag}
+
+# for arm based MACs use the following:
+docker buildx build -t "${full_image_name}" . --platform linux/amd64
+
+# for x86 based machines use the following:
+# docker build -t ${full_image_name} .
+
+docker push ${full_image_name}
+
+# Output the strict image name, which contains the sha256 image digest
+docker inspect --format="{{index .RepoDigests 0}}" "${full_image_name}"
